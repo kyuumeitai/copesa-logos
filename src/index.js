ac73c058a77f71197bf7531db9a391b65d711eb9
Álex Acuña Viera <kyuumeitai@gmail.com>
2019-02-11 11:42:00 -0300

    color fill ok in lab logo

--- src/index.js
+++ src/index.js
@@ -1,21 +1,57 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import Latercera from './logos/Latercera'
+import Beethoven from './logos/Beethoven'
+import Biut from './logos/Biut'
+import Diarioconcepcion from './logos/Diarioconcepcion'
+import Duna from './logos/Duna'
+import Glamorama from './logos/Glamorama'
+import Grupocopesa from './logos/Grupocopesa'
+import GrupocopesaAlt from './logos/GrupocopesaAlt'
+import Icarito from './logos/Icarito'
+import LaboratorioDeContenidosDeMarca from './logos/LaboratorioDeContenidosDeMarca'
+import Lacuarta from './logos/Lacuarta'
+import LahoraTubuenanoticia from './logos/LahoraTubuenanoticia'
+import LtEldeportivo from './logos/LtEldeportivo'
+import LtFinde from './logos/LtFinde'
+import LtNegocios from './logos/LtNegocios'
+import LtPulso from './logos/LtPulso'
+import LtReportajes from './logos/LtReportajes'
+import LtTendencias from './logos/LtTendencias'
+import Masdeco from './logos/Masdeco'
+import MediosDigitalesGrupocopesa from './logos/MediosDigitalesGrupocopesa'
+import Mouse from './logos/Mouse'
+import Mtonline from './logos/Mtonline'
+import Paula from './logos/Paula'
+import Quepasa from './logos/Quepasa'
+import Radiodisney from './logos/Radiodisney'
+import Zero from './logos/Zero'
+import ZeroAlt from './logos/ZeroAlt'
 
-export default class LTLogo extends Component {
-  static propTypes = {
-    icon: PropTypes.string,
-    size: PropTypes.number,
-    className: PropTypes.string
-  }
-
-  render() {
-    const {
-      icon,
-      className,
-      size = 40
-    } = this.props
-
-    return <Latercera />
-  }
+export {
+  Latercera,
+  Beethoven,
+  Biut,
+  Diarioconcepcion,
+  Duna,
+  Glamorama,
+  Grupocopesa,
+  GrupocopesaAlt,
+  Icarito,
+  LaboratorioDeContenidosDeMarca,
+  Lacuarta,
+  LahoraTubuenanoticia,
+  LtEldeportivo,
+  LtFinde,
+  LtNegocios,
+  LtPulso,
+  LtReportajes,
+  LtTendencias,
+  Masdeco,
+  MediosDigitalesGrupocopesa,
+  Mouse,
+  Mtonline,
+  Paula,
+  Quepasa,
+  Radiodisney,
+  Zero,
+  ZeroAlt
 }
