b960983d477f1605d2cb7b79570996ee91e4e129
Fabian <fabian@fabz.tv>
2019-08-12 15:28:39 -0400

    bug fix y new logos

---
+++ src/logos/all/ZeroDialWhite.js
@@ -0,0 +1,87 @@
+import React from "react";
+
+const SvgLogoZerodialWhite = props => (
+  <svg
+    id="Logo_zerodial_white_svg__Layer_1"
+    x={0}
+    y={0}
+    viewBox="0 0 1442.2 235.4"
+    xmlSpace="preserve"
+    {...props}
+  >
+    <style>{".Logo_zerodial_white_svg__st1{fill:#fff}"}</style>
+    <path
+      d="M719.3.5C654.4.5 601.6 53.2 601.6 118s52.8 117.5 117.7 117.5S837 182.7 837 118 784.2.5 719.3.5zM815.5 118c0 52.9-43.2 96-96.2 96S623 170.9 623 118s43.2-96 96.2-96 96.3 43 96.3 96z"
+      fill="#f92e22"
+    />
+    <path
+      className="Logo_zerodial_white_svg__st1"
+      d="M210.5 234.4H367v-22.2H232.6v-83.9H367v-22.2H232.6v-84H367V0H210.5zM480.7 128.3h29.7c15.1 0 29.8-5.4 41.3-15.1 14.5-12.2 22.8-30.1 22.8-49s-8.3-36.8-22.8-49C540.2 5.4 525.5 0 510.4 0H418v234.4h22.1V128.3h10l99.4 106.1h30.2l-99-106.1zm71.7-64.2c0 23.2-18.8 42-42 42h-70.3v-84h70.3c23.2 0 42 18.9 42 42zM2.9 0v22.1H123L0 234.4h159.5v-22.2H39.4L162.4 0zM1158.2 0l-135.8 234.4h26.6L1184.8 0z"
+    />
+    <path
+      className="Logo_zerodial_white_svg__st1"
+      d="M1027.9 0h142.5v22.1h-142.5zM1339.4 0l-135.8 234.4h26.7L1366.1 0z"
+    />
+    <path
+      className="Logo_zerodial_white_svg__st1"
+      d="M1209.2 0h142.5v22.1h-142.5zM948.8 139.6l-.2.3h-.2l-54.7 94.4h26.6l74.6-128.8c-9.7 17.2-26.4 29.7-46.1 34.1z"
+    />
+    <path
+      className="Logo_zerodial_white_svg__st1"
+      d="M1000.9 49.3C991.8 20.7 965.1 0 933.5 0c-39.1 0-70.8 31.7-70.8 70.8 0 39.1 31.7 70.8 70.8 70.8 5.1 0 10.1-.6 14.9-1.6l.2-.3 52.3-90.4zm-115 21.5c0-26.2 21.3-47.5 47.5-47.5S981 44.5 981 70.8s-21.3 47.5-47.5 47.5-47.6-21.3-47.6-47.5z"
+    />
+    <path
+      className="Logo_zerodial_white_svg__st1"
+      d="M948.8 139.6h-.2l-.2.3h.2l.2-.3zM948.8 139.6c19.7-4.4 36.4-16.9 46.1-34l.1-.2c5.8-10.3 9.2-22.2 9.2-34.9 0-7.4-1.1-14.6-3.3-21.3l-.1.1c0 .1 0 .1.1.2l-52.1 90.1z"
+    />
+    <path
+      className="Logo_zerodial_white_svg__st1"
+      d="M990.2 105.4l-.1.2c0-.1.1-.2.1-.2zM948.8 139.6l52.2-90.1c0-.1 0-.1-.1-.2l-52.3 90.3h.2z"
+    />
+    <circle
+      className="Logo_zerodial_white_svg__st1"
+      cx={933.5}
+      cy={70.8}
+      r={47.5}
+    />
+    <path
+      className="Logo_zerodial_white_svg__st1"
+      d="M1317.9 168.7h13.4v65.7h-13.4z"
+    />
+    <path
+      transform="rotate(90 1341.99 175.232)"
+      className="Logo_zerodial_white_svg__st1"
+      d="M1335.4 151.2h13.1v48.2h-13.1z"
+    />
+    <path
+      transform="rotate(90 1338.084 201.528)"
+      className="Logo_zerodial_white_svg__st1"
+      d="M1331.5 181.4h13.1v40.3h-13.1z"
+    />
+    <path
+      className="Logo_zerodial_white_svg__st1"
+      d="M1382.5 234.4h11.9v-32.9l-11.9-13.1zM1430.3 234.4h11.9v-46l-11.9 13.1z"
+    />
+    <path
+      className="Logo_zerodial_white_svg__st1"
+      d="M1430.3 181.8l-17.9 19.7-18-19.7v19.7l18 19.7 17.9-19.7v-11.8z"
+    />
+    <path
+      className="Logo_zerodial_white_svg__st1"
+      d="M1382.5 168.7v19.7l11.9 13.1v-19.7zM1430.3 201.5l11.9-13.1v-6.6l-11.9 7.9z"
+    />
+    <path
+      className="Logo_zerodial_white_svg__st1"
+      d="M1430.3 181.8v7.9l11.9-7.9v-13.1z"
+    />
+    <circle
+      className="Logo_zerodial_white_svg__st1"
+      cx={1134.9}
+      cy={220.7}
+      r={13.6}
+    />
+  </svg>
+);
+
+export default SvgLogoZerodialWhite;
+
