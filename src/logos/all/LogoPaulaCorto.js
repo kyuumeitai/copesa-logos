19e9d440a2ba42e237bfad283e4d6beef8885c9c
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-04-02 21:31:43 -0300

    2.0 clean initialize

--- src/logos/all/LogoPaulaCorto.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SvgLogoPaulaCorto = props => (
-  <svg data-name="Layer 1" viewBox="0 0 200 200.01" {...props}>
-    <path d="M72.11 48.44h27c16.28 0 27.48 6.13 27.48 27.05 0 14.79-8.67 25.36-26.42 25.36H72.11zm-28.32 126.8h28.32v-50.72h33.39c29.79 0 50.71-19.23 50.71-49 0-29.16-17.33-50.72-52.62-50.72h-59.8z" />
-  </svg>
-);
-
-export default SvgLogoPaulaCorto;
