8564facd51bfd7a5b920904aad403d6e6dcd847e
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-04-03 12:53:32 -0300

    no console

--- src/logos/Laboratorio/Circle.jsx
+++ src/logos/Laboratorio/Circle.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 function SvgLaboratorioCircleBnFull(props) {
-  console.log("props!", props);
   return (
     <svg viewBox="0 0 137.98 137.98" {...props}>
       <defs>
