8ea7ffc23cb710719ddc05db54d9a239e33b6e43
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-09-23 16:38:36 -0300

    (feat) new logo: Práctico

---
+++ src/logos/Practico/index.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Practico from ".";
+
+export default {
+  title: "Logos/Práctico",
+  component: Practico,
+  argTypes: {
+    backgroundColor: { control: "color" }
+  }
+};
+
+const Template = args => <Practico {...args} />;
+
+export const Common = Template.bind({});
+Common.args = {
+  schema: "common"
+};
+
+export const Light = Template.bind({});
+Light.args = {
+  schema: "light"
+};
