d73bfa9d8235f8189b3c2410bbe5006d865dc556
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-09-23 16:21:01 -0300

    (feat) new logo Finde

---
+++ src/logos/Finde/index.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Finde from ".";
+
+export default {
+  title: "Logos/Finde",
+  component: Finde,
+  argTypes: {
+    backgroundColor: { control: "color" }
+  }
+};
+
+const Template = args => <Finde {...args} />;
+
+export const Common = Template.bind({});
+Common.args = {
+  schema: "common"
+};
+
+export const Light = Template.bind({});
+Light.args = {
+  schema: "light"
+};
