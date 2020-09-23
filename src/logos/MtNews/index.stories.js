adb62d862facd0e64df47172b5b7847c112b1de8
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-09-23 13:49:14 -0300

    (feat) New Logo

---
+++ src/logos/MtNews/index.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import MtNews from ".";
+
+export default {
+  title: "Logos/MtNews",
+  component: MtNews,
+  argTypes: {
+    backgroundColor: { control: "color" }
+  }
+};
+
+const Template = args => <MtNews {...args} />;
+
+export const Common = Template.bind({});
+Common.args = {
+  schema: "common"
+};
+
+export const Light = Template.bind({});
+Light.args = {
+  schema: "light"
+};
