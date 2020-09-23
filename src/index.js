89e8d1d8110f540fd63785465021c5cd8eb9fcd2
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-09-23 18:46:59 -0300

    Export logos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
+import ClubLaTercera from "./logos/ClubLaTercera";
+import Coronavirus from "./logos/Coronavirus";
+import Culto from "./logos/Culto";
+import Duna from "./logos/Duna";
+import FactChecking from "./logos/FactChecking";
+import Finde from "./logos/Finde";
+import Interactivo from "./logos/Interactivo";
 import Laboratorio from "./logos/Laboratorio";
 import LaTercera from "./logos/LaTercera";
+import LaTerceraAM from "./logos/LaTerceraAM";
+import LaTerceraPM from "./logos/LaTerceraPM";
+import LaTerceraDomingo from "./logos/LaTerceraDomingo";
+import MasDeco from "./logos/MasDeco";
+import Mouse from "./logos/Mouse";
+import MtNews from "./logos/MtNews";
 import MtOnline from "./logos/MtOnline";
+import Paula from "./logos/Paula";
+import PaulaCocina from "./logos/PaulaCocina";
+import PiensaDigital from "./logos/PiensaDigital";
+import Practico from "./logos/Practico";
+import Pulso from "./logos/Pulso";
+import PulsoTrader from "./logos/PulsoTrader";
+import QuePasa from "./logos/QuePasa";
+import ReConstitucion from "./logos/ReConstitucion";
 
-const Logos = {
+export {
+  ClubLaTercera,
+  Coronavirus,
+  Culto,
+  Duna,
+  FactChecking,
+  Finde,
+  Interactivo,
   Laboratorio,
   LaTercera,
-  MtOnline
+  LaTerceraAM,
+  LaTerceraPM,
+  LaTerceraDomingo,
+  MasDeco,
+  Mouse,
+  MtNews,
+  MtOnline,
+  Paula,
+  PaulaCocina,
+  PiensaDigital,
+  Practico,
+  Pulso,
+  PulsoTrader,
+  QuePasa,
+  ReConstitucion
 };
-
-export { Laboratorio, LaTercera, MtOnline };
-
-export default Logos;
