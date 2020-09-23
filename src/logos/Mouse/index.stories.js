e47a72ea0b4aab93493f66004516c7d02892472a
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-09-23 16:29:19 -0300

    (feat) new logo Mouse

---
+++ src/logos/Mouse/index.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Mouse from ".";
+
+export default {
+  title: "Logos/Mouse",
+  component: Mouse,
+  argTypes: {
+    backgroundColor: { control: "color" }
+  }
+};
+
+const Template = args => <Mouse {...args} />;
+
+export const Common = Template.bind({});
+Common.args = {
+  schema: "common"
+};
+
+export const Light = Template.bind({});
+Light.args = {
+  schema: "light"
+};
