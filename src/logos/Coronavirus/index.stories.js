c7c9e770e28e1cf6c9492271dd21cc9ea1369903
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-09-23 17:01:03 -0300

    (feat) new logo: Coronavirus

---
+++ src/logos/Coronavirus/index.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Coronavirus from ".";
+
+export default {
+  title: "Logos/Coronavirus",
+  component: Coronavirus,
+  argTypes: {
+    backgroundColor: { control: "color" }
+  }
+};
+
+const Template = args => <Coronavirus {...args} />;
+
+export const Common = Template.bind({});
+Common.args = {
+  schema: "common"
+};
+
+export const Light = Template.bind({});
+Light.args = {
+  schema: "light"
+};
