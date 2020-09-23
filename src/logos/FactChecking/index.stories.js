3f0a7d105be66071237a270e320d463767ed0009
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-09-23 18:04:07 -0300

    (feat) new logo: Fact Checking

---
+++ src/logos/FactChecking/index.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import FactChecking from ".";
+
+export default {
+  title: "Logos/Fact Checking",
+  component: FactChecking,
+  argTypes: {
+    backgroundColor: { control: "color" }
+  }
+};
+
+const Template = args => <FactChecking {...args} />;
+
+export const Common = Template.bind({});
+Common.args = {
+  schema: "common"
+};
+
+export const Light = Template.bind({});
+Light.args = {
+  schema: "light"
+};
