8661b7dfd04bb210371afae3cf56c09484624d02
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-09-23 14:03:38 -0300

    (feat) New logo: Culto

---
+++ src/logos/Culto/index.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Culto from ".";
+
+export default {
+  title: "Logos/Culto",
+  component: Culto,
+  argTypes: {
+    backgroundColor: { control: "color" }
+  }
+};
+
+const Template = args => <Culto {...args} />;
+
+export const Common = Template.bind({});
+Common.args = {
+  schema: "common"
+};
+
+export const Light = Template.bind({});
+Light.args = {
+  schema: "light"
+};
