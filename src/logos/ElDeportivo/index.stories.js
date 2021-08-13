918ff84f682b43b55f09fae1242d7e2eede9b052
Álex Acuña Viera @ Nataku WSL2 <kyumeitai@gmail.com>
2021-08-13 16:46:39 -0400

    (feat) new logo El Deportivo

---
+++ src/logos/ElDeportivo/index.stories.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import ElDeportivo from '.'
+
+export default {
+  title: 'Logos/El Deportivo',
+  component: ElDeportivo,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+}
+
+const Template = args => <ElDeportivo {...args} />
+
+export const Common = Template.bind({})
+Common.args = {
+  schema: 'common',
+}
+
+export const Light = Template.bind({})
+Light.args = {
+  schema: 'light',
+}
