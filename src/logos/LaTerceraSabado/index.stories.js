d9079a925aa2821b667997870df07688c881a74a
Álex Acuña Viera @ Nataku WSL2 <kyumeitai@gmail.com>
2021-08-24 18:34:01 -0400

    (chore) LaTerceraSabado small

--- src/logos/LaTerceraSabado/index.stories.js
+++ src/logos/LaTerceraSabado/index.stories.js
@@ -3,7 +3,7 @@ import React from 'react'
 import LaTerceraSabado from '.'
 
 export default {
-  title: 'Logos/LaTerceraSabado',
+  title: 'Logos/La Tercera Sábado',
   component: LaTerceraSabado,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -14,10 +14,24 @@ const Template = args => <LaTerceraSabado {...args} />
 
 export const Common = Template.bind({})
 Common.args = {
+  version: 'large',
   schema: 'common',
 }
 
 export const Light = Template.bind({})
 Light.args = {
+  version: 'large',
+  schema: 'light',
+}
+
+export const CommonSmall = Template.bind({})
+CommonSmall.args = {
+  version: 'small',
+  schema: 'common',
+}
+
+export const LightSmall = Template.bind({})
+LightSmall.args = {
+  version: 'small',
   schema: 'light',
 }
