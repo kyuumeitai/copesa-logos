4f9698835fe471f51e920b6c0f79798fbd17f735
Álex Acuña Viera <kyuumeitai@gmail.com>
2022-02-22 17:46:32 -0300

    feat: La Cuarta New

--- src/logos/LaCuarta/index.stories.js
+++ src/logos/LaCuarta/index.stories.js
@@ -1,23 +1,68 @@
-import React from "react";
+import React from 'react'
 
-import LaCuarta from ".";
+import LaCuarta from '.'
 
 export default {
-  title: "Logos/LaCuarta",
+  title: 'Logos/LaCuarta',
   component: LaCuarta,
   argTypes: {
-    backgroundColor: { control: "color" }
-  }
-};
+    backgroundColor: { control: 'color' },
+  },
+}
 
-const Template = args => <LaCuarta {...args} />;
+const Template = args => <LaCuarta {...args} />
 
-export const Common = Template.bind({});
+export const Common = Template.bind({})
 Common.args = {
-  schema: "common"
-};
+  schema: 'common',
+}
 
-export const Light = Template.bind({});
+export const Light = Template.bind({})
 Light.args = {
-  schema: "light"
-};
+  schema: 'light',
+}
+
+export const Claim = Template.bind({})
+Claim.args = {
+  version: 'claim',
+  orientation: 'vertical',
+  schema: 'common',
+}
+
+export const Vertical = Template.bind({})
+Vertical.args = {
+  orientation: 'vertical',
+  schema: 'common',
+}
+
+export const Horizontal = Template.bind({})
+Horizontal.args = {
+  orientation: 'horizontal',
+  schema: 'common',
+}
+
+export const Comerciante = Template.bind({})
+Comerciante.args = {
+  version: 'comerciante',
+  orientation: 'vertical',
+  schema: 'common',
+}
+
+export const Constructor = Template.bind({})
+Constructor.args = {
+  version: 'constructor',
+  orientation: 'vertical',
+  schema: 'common',
+}
+
+export const Icon = Template.bind({})
+Icon.args = {
+  version: 'icon',
+  schema: 'common',
+}
+
+export const Sello = Template.bind({})
+Sello.args = {
+  version: 'sello',
+  schema: 'common',
+}
