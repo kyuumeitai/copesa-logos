48caac6fb6b7d3b99aeb157721f1b5fc6e51a59c
Ålex Acuña Viera <kyuumeitai@gmail.com>
2022-11-09 14:47:56 -0300

    bring jsx

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
+  features: {
+    storyStoreV7: true,
+  },
   core: {
-    builder: 'webpack5',
+    builder: '@storybook/builder-webpack5',
   },
 }
